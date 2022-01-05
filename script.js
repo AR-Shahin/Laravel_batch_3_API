8e69f04cfd73466ed4815dbba49224675c77db96
AR-Shahin <mdshahinmije96@gmail.com>
2022-01-05 18:17:48 +0600

    bd

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "shahin" && b=="06-01-2000")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
